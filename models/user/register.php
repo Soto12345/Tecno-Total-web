8673b83963d0dc4733d2806620eda5748bc2fc47
Soto <a22110111@ceti.mx>
2023-11-24 19:15:35 -0600

    modificacion pendeja de la direccion de registro

--- models/user/register.php
+++ models/user/register.php
@@ -6,6 +6,6 @@
     $user->setPassword_user($_POST['password_user']);
     register_user($user->getEmail_user(),$user->getPassword_user());
     //retorna otra vez a la pagina de registro
-    header('location ../../Register.html');
+    header('location: ../forms/Register.html');
     exit();
 ?>
