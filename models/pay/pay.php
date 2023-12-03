6baf310933ead293b9a31fc82afd7870a8052b16
Fabian <arnolfabian0@gmail.com>
2023-12-03 02:31:43 -0600

    Diseño de pagina pay (Terminado)

--- models/pay/pay.php
+++ models/pay/pay.php
@@ -2,59 +2,71 @@
 function pay()
 {
     $total = 0;
-        foreach ($_SESSION['CART'] as $index => $product) {
-            $total = $total + ($product['product_stock'] * $product['product_price']);
-        }
+    foreach ($_SESSION['CART'] as $index => $product) {
+        $total = $total + ($product['product_stock'] * $product['product_price']);
+    }
 ?>
-    <h5>cuenta para prueba: sb-sh1ji27450968@business.example.com</h5>
-    <h5>contraseña de la cuenta: iBI3!Pl2</h5>
-    <h1>!ETAPA DE PAGO¡</h1>
-    <hr>
-    <p>
-        En esta etapa se realizara el pago de:
-    <h3>$<?php echo number_format($total, 2) ?></h3>
-    <div id="paypal-button-container"></div>
-    </p>
-    <script>
-        paypal.Buttons({
-            style: {
-                color: 'blue',
-                shape: 'pill',
-                label: 'pay'
+    <div class="container mt-5">
+        <div class="card">
+            <div class="card-body">
+                <h5 class="card-title">Cuenta para prueba:</h5>
+                <p class="card-text">sb-sh1ji27450968@business.example.com</p>
+                <h5 class="card-title">Contraseña de la cuenta:</h5>
+                <p class="card-text">iBI3!Pl2</p>
+            </div>
+        </div>
 
-            },
-            createOrder: function(data, actions) {
-                return actions.order.create({
-                    purchase_units: [{
-                        amount: {
-                            value: <?php echo $total ?>
+        <div class="card mt-3">
+            <div class="card-body">
+                <h1 class="card-title text-white bg-primary p-2">¡ETAPA DE PAGO!</h1>
+                <hr>
+                <p class="card-text">
+                    En esta etapa se realizará el pago de:
+                    <h3>$<?php echo number_format($total, 2) ?></h3>
+                </p>
 
+                <!-- Contenedor para centrar los botones -->
+                <div class="d-flex justify-content-center">
+                    <div id="paypal-button-container"></div>
+                </div>
+                <script>
+                    paypal.Buttons({
+                        style: {
+                            color: 'blue',
+                            shape: 'pill',
+                            label: 'pay'
                         },
-                    }]
-
-                });
-            },
-            onApprove: function(data, actions) {
-                actions.order.capture().then(function(detalles) {
-                    console.log(detalles);
-                    console.log(data);
-                    alert("PAGO EXITOSO");
-                    alert("PAGO EN PROCESO, POR FAVOR ESPERE");
-                    window.location = "models/pay/ticket.php?paymentID=" + data.paymentID;
-
-                });
-
-            },
-            onCancel: function(data) {
-                alert("Pago cancelado");
-                console.log(data);
-            },
-            onError: function(err) {
-                // Manejo de errores
-                console.error(err);
-            }
-        }).render('#paypal-button-container');
-    </script>
+                        createOrder: function(data, actions) {
+                            return actions.order.create({
+                                purchase_units: [{
+                                    amount: {
+                                        value: <?php echo $total ?>
+                                    },
+                                }]
+                            });
+                        },
+                        onApprove: function(data, actions) {
+                            actions.order.capture().then(function(detalles) {
+                                console.log(detalles);
+                                console.log(data);
+                                alert("PAGO EXITOSO");
+                                alert("PAGO EN PROCESO, POR FAVOR ESPERE");
+                                window.location = "models/pay/ticket.php?paymentID=" + data.paymentID;
+                            });
+                        },
+                        onCancel: function(data) {
+                            alert("Pago cancelado");
+                            console.log(data);
+                        },
+                        onError: function(err) {
+                            // Manejo de errores
+                            console.error(err);
+                        }
+                    }).render('#paypal-button-container');
+                </script>
+            </div>
+        </div>
+    </div>
 <?php
 }
 ?>
