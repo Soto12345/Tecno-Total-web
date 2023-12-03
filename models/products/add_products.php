85ca449a5c3893e3600d52c93b3bb8822c4f965c
Fabian <arnolfabian0@gmail.com>
2023-12-03 02:48:51 -0600

    Diseño para agregar productos(Terminado)

--- models/products/add_products.php
+++ models/products/add_products.php
@@ -2,33 +2,53 @@
 function add_products()
 {
 ?>
-    <h1>Agregar producto</h1>
-    <form method="POST">
-        <input type="text" name="product_name" id="product_name" placeholder="Escribe Nombre del producto" required>
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Agregar producto</h1>
+            <form method="POST">
+                <div class="form-group">
+                    <label for="product_name">Nombre del producto:</label>
+                    <input type="text" class="form-control" name="product_name" id="product_name" placeholder="Escribe el nombre del producto" required>
+                </div>
 
-        <input type="text" name="product_description" id="product_description" placeholder="Escribe la descripcion del producto" required>
+                <div class="form-group">
+                    <label for="product_description">Descripción del producto:</label>
+                    <input type="text" class="form-control" name="product_description" id="product_description" placeholder="Escribe la descripción del producto" required>
+                </div>
 
-        <input type="number" name="product_price" id="product_price" placeholder="Escribe el precio del producto" required>
+                <div class="form-group">
+                    <label for="product_price">Precio del producto:</label>
+                    <input type="number" class="form-control" name="product_price" id="product_price" placeholder="Escribe el precio del producto" required>
+                </div>
 
-        <label for="options">Categoria:</label>
-        <select id="options" name="product_category" required>
-            <option value="almacenamiento.hdd">almacenamiento-hdd</option>
-            <option value="almacenamiento.ssd">almacenamiento-ssd</option>
-            <option value="componentes.grafica">componentes-grafica</option>
-            <option value="componentes.ram">componentes-ram</option>
-            <option value="componentes.motherboard">componentes-motherboard</option>
-            <option value="dispositivos.teclado">dispositivos-teclado</option>
-            <option value="dispositivos.monitor">dispositivos-monitor</option>
-            <option value="dispositivos.mouse">dispositivos-mouse</option>
-        </select>
+                <div class="form-group">
+                    <label for="options">Categoría:</label>
+                    <select class="form-control" id="options" name="product_category" required>
+                        <option value="almacenamiento.hdd">Almacenamiento HDD</option>
+                        <option value="almacenamiento.ssd">Almacenamiento SSD</option>
+                        <option value="componentes.grafica">Componentes Gráficos</option>
+                        <option value="componentes.ram">Componentes RAM</option>
+                        <option value="componentes.motherboard">Componentes Motherboard</option>
+                        <option value="dispositivos.teclado">Dispositivos Teclado</option>
+                        <option value="dispositivos.monitor">Dispositivos Monitor</option>
+                        <option value="dispositivos.mouse">Dispositivos Mouse</option>
+                    </select>
+                </div>
 
-        <input type="text" name="product_image" id="product_image" placeholder="agrega el url de la imagen">
+                <div class="form-group">
+                    <label for="product_image">URL de la imagen:</label>
+                    <input type="text" class="form-control" name="product_image" id="product_image" placeholder="Agrega el URL de la imagen">
+                </div>
 
-        <input type="number" name="product_stock" id="product_stock" placeholder="Escribe la cantidad a poner para el producto" required>
+                <div class="form-group">
+                    <label for="product_stock">Cantidad en stock:</label>
+                    <input type="number" class="form-control" name="product_stock" id="product_stock" placeholder="Escribe la cantidad en stock" required>
+                </div>
 
-        <button name="btnAdd" type="submit">Enviar</button>
-    </form>
+                <button name="btnAdd" type="submit" class="btn btn-primary">Enviar</button>
+            </form>
+        </div>
+    </div>
 <?php
 }
-
 ?>
