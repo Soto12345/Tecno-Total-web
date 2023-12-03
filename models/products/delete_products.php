a40bcc560354af12026cd119a8215a1b1ee9ccc5
Fabian <arnolfabian0@gmail.com>
2023-12-03 02:51:04 -0600

    Diseño de eliminar producto (terminado)

--- models/products/delete_products.php
+++ models/products/delete_products.php
@@ -5,25 +5,29 @@ function delete_product()
     $sql_name = "SELECT Nombre_producto FROM producto";
     $result_name = $connection->run_query($sql_name);
 ?>
-    <h1>Eliminar producto</h1>
+    <div class="card">
+        <div class="card-body">
+            <h1 class="card-title">Eliminar producto</h1>
 
+            <form method="POST">
+                <div class="form-group">
+                    <label for="options">Nombres:</label>
+                    <select class="form-control" id="options" name="product_name" required>
+                        <?php
+                        while ($fila = mysqli_fetch_assoc($result_name)) {
+                            $name = $fila['Nombre_producto'];
+                        ?>
+                            <option value='<?php echo $name ?>'><?php echo $name ?></option>
+                        <?php
+                        }
+                        ?>
+                    </select>
+                </div>
 
-    <form method="POST">
-        <label for="options">nombres:</label>
-        <select id="options" name="product_name" required>
-            <?php
-            while ($fila = mysqli_fetch_assoc($result_name)) {
-                $name = $fila['Nombre_producto'];
-            ?>
-                <option value='<?php echo $name ?>'><?php echo $name ?></option>
-            <?php
-            }
-            ?>
-
-        </select>
-        <button name="btnDelete" type="submit">Enviar</button>
-    </form>
+                <button name="btnDelete" type="submit" class="btn btn-danger">Eliminar</button>
+            </form>
+        </div>
+    </div>
 <?php
 }
-
 ?>
